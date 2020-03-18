81c3adeb8c3d14d13e6ce496377f6af33418c20f
Nicole Tilbe <nicolecali2406@outlook.com>
2020-03-18 19:55:11 -0400

    Added header files and class

--- Asteroids Game/Asteroids Game/Asteroids Game.cpp
+++ Asteroids Game/Asteroids Game/Asteroids Game.cpp
@@ -1,1 +1,16 @@
 // Asteroids Game.cpp 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#include "olcConsoleGameEngine.h"
+
+class OneLoneCoder_Asteroids : public olcConsoleGameEngine
+{
+
+
+
+
+};
